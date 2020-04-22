80d7c3833600b993a313e0972ad69e2d454960a5
Felix Rodriguez <frod25@gmail.com>
2020-04-22 20:04:29 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,17 @@
-# don't forget to add: require 'pry'
+p1 = "Bill"
+p2 = "Bob"
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  greeting = "Hello #{player1} & #{player2}"
+  puts greeting
+  greeting
 end
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+def play_game(player1, player2)
+  whos_better = "#{player1} is better than #{player2}"
+  puts whos_better
+  whos_better
 end
+
+start_game(p1, p2)
+play_game(p1, p2)
