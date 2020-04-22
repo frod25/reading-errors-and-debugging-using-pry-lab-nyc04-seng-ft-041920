e50a872a8a3d15692487700152aeb1e1ad1d7266
Felix Rodriguez <frod25@gmail.com>
2020-04-22 19:44:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,8 +2,10 @@
 
 def start_game(player1, player2)
   greeting = "Hello #{player1} & #{playar2}"
+  greeting
 end
 
 def play_game(player2)
   whos_better = "#{player1} is better than #{player2}"
+  whos_better
 end
