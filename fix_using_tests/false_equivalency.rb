289c5ef871d50aa1de88c3b65040903fc332f1b9
Felix Rodriguez <frod25@gmail.com>
2020-04-22 19:39:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,3 @@
-require 'pry'
 
 def get_user_input
   gets.chomp
@@ -8,7 +7,6 @@ def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
-  binding.pry
 end
 
 def selection(num)
@@ -24,4 +22,4 @@ def runner
   selection(get_user_input)
 end
 
-runner
+
