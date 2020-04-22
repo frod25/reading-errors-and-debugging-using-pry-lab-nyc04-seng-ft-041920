c3be3339b62b53cb585cab325ed7b2943ece8639
Felix Rodriguez <frod25@gmail.com>
2020-04-22 19:50:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -9,7 +9,7 @@ def start_game(player1, player2)
   binding.pry
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   whos_better = "#{player1} is better than #{player2}"
   whos_better
   binding.pry
