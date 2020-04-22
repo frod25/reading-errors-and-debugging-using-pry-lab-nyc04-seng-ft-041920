acdef072ccafec80457e91cbdf4ef8a77be496aa
Felix Rodriguez <frod25@gmail.com>
2020-04-22 19:51:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -6,14 +6,12 @@ p2 = "Bob"
 def start_game(player1, player2)
   greeting = "Hello #{player1} & #{player2}"
   greeting
-  binding.pry
 end
 
 def play_game(player1, player2)
   whos_better = "#{player1} is better than #{player2}"
   whos_better
-  binding.pry
 end
-
+binding.pry
 start_game(p1, p2)
 play_game(p1, p2)
