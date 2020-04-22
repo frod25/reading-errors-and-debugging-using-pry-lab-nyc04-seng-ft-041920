163690653d5a030f5ef6a9f4ed83e3f8eddbe897
Felix Rodriguez <frod25@gmail.com>
2020-04-22 19:47:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,11 +1,19 @@
-# don't forget to add: require 'pry'
+require 'pry'
+
+p1 = "Bill"
+p2 = "Bob"
 
 def start_game(player1, player2)
   greeting = "Hello #{player1} & #{playar2}"
   greeting
+  binding.pry
 end
 
 def play_game(player2)
   whos_better = "#{player1} is better than #{player2}"
   whos_better
+  binding.pry
 end
+
+start_game(p1, p2)
+play_game(p2)
